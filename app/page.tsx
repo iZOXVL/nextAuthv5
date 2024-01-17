9419fbc25e2e93e5852ed2a4c717c5fd3a33b3d5
Armando VL <103782831+iZOXVL@users.noreply.github.com>
2024-01-17 13:56:40 -0600

    Lottie and Framer Motion Implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
 import { Poppins } from "next/font/google"
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button"
 import { LoginButton } from "@/components/auth/login-button";
+import authAnimation from "@/resources/lottie/authAnimation.json"
+import Lottie from "lottie-react";
+import {motion} from "framer-motion";
 
 const font = Poppins({
  subsets: ["latin"],
@@ -9,21 +13,25 @@ const font = Poppins({
 });
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center 
+    <main className="flex h-full flex-col items-center  
     bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-purple-900 to-indigo-700">
     <div className="space-y-6">
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+    animate={{ opacity: 1, scale: 1 }}
+    transition={{ duration: 0.5 }} className="w-[350px]  h-[350px] ">
+     <Lottie animationData={authAnimation}/>
+     </motion.div>
+
       <h1 className={cn(
-        "text-6xl font-semibold text-white drop-shadow-md",
+        "text-2xl font-semibold text-white drop-shadow-md text-center",
         font.className, 
       )}>
-        🔐Auth
-      </h1>
-      <p className="text-white text-lg text-center">
         Servicio de autenticacion
-      </p>
-      <div className="justify-center text-center">
+      </h1>
+      <div className="text-center">
         <LoginButton>
-        <Button variant="secondary" size="lg">
+        <Button variant="secondary" size="xl">
           Acceder
         </Button>
         </LoginButton>
