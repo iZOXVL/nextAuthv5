de98eaa70f387c40743618411d795e18dd38d3f2
Armando VL <103782831+iZOXVL@users.noreply.github.com>
2024-01-18 16:35:06 -0600

    Register Funcionality

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 "use server";
 
+import { db } from "@/lib/db";
+import bcrypt from "bcryptjs";
 import * as z from "zod";
 
 import {RegisterSchema} from "@/schemas";
+import { getUserByEmail } from "@/data/users";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
    const validateFielda = RegisterSchema.safeParse(values);
@@ -11,5 +14,27 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return{error: "Credenciales inavlidas"}
    }
 
-    return{success: "Correo de verificación enviado"}
+   const {email, password, name} = validateFielda.data;
+   const hashedPassword = await bcrypt.hash(password, 10);
+   
+   const existingUser = await getUserByEmail(email);
+
+   if (existingUser){
+    return {error: "El correo ya está siendo utilizado"};
+    }
+   
+    await db.user.create ({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        },
+    });
+
+    //TODO: ENVIAR CORREO DE VERIFICACION AL CORREO ENEVIADO
+
+
+
+
+    return{success: "Usuario creado"}
 };
