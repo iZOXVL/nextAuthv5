8db248ce1b16205cb1d8f966009069ef5104dd5a
Armando VL <103782831+iZOXVL@users.noreply.github.com>
2024-01-23 20:50:51 -0600

    OAuth 2.0 50%

--- auth.config.ts
+++ auth.config.ts
@@ -3,9 +3,24 @@ import type { NextAuthConfig } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import { LoginSchema } from "@/schemas"
 import { getUserByEmail } from "@/data/users"
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Spotify from "next-auth/providers/spotify";
 
 export default {
   providers: [
+    Spotify({
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials){
         const validateFields = LoginSchema.safeParse(credentials);
--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import {PrismaAdapter} from "@auth/prisma-adapter";
-import authConfig from "./auth.config"
-import { db } from "./lib/db";
+import authConfig from "@/auth.config"
+import { db } from "@/lib/db";
 import { getUserById } from "@/data/users";
 import { UserRole } from "@prisma/client";
 
